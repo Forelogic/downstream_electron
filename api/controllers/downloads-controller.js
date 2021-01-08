f404e8e9a6f13d40397ff56c9baf3fe47dc939ca
KazukiSugiyama107 <sugiyama@forelogic.co.jp>
2021-01-08 11:44:41 +0900

    ログ追加

--- api/controllers/downloads-controller.js
+++ api/controllers/downloads-controller.js
@@ -212,7 +212,7 @@ DownloadsController.prototype._markDownloadItem = function (download) {
     syncStorageKeys.push(this.storage.stores.DOWNLOADS.DOWNLOADED);
   } else {
     self.storage.errors.push(manifestId, download);
-    log.info('downloads-controller _markDownloadItem : ', self.storage.errors._items)
+    log.info('downloads-controller _markDownloadItem : ', self.storage.errors._parent._items)
   }
   self.storage.downloading.removeItem(manifestId, downloadHash);
 
@@ -594,9 +594,9 @@ DownloadsController.prototype.start = function (manifestId, representations, dow
 
           self.storage.downloaded.clear(manifestId);
           self.storage.downloaded.concat(manifestId, downloaded);
-          log.info('before storage.errors.clear() : ', self.storage.errors._items)
+          log.info('before storage.errors.clear() : ', self.storage.errors._parent._items)
           self.storage.errors.clear(manifestId);
-          log.info('after storage.errors.clear() : ', self.storage.errors._items)
+          log.info('after storage.errors.clear() : ', self.storage.errors._parent._items)
 
           if (!fromResumed) {
             // do not initialise state of manifest is start is from resume
