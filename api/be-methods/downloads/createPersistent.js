ea59d33e01909090ee3a3e727603bd0787bd5a60
KazukiSugiyama107 <sugiyama@forelogic.co.jp>
2021-01-13 19:05:05 +0900

    ログ追加

--- api/be-methods/downloads/createPersistent.js
+++ api/be-methods/downloads/createPersistent.js
@@ -1,5 +1,7 @@
 "use strict";
+const log = require('electron-log');
 
 module.exports = function (api, onSuccess /*, onFailure, target, manifestId */) {
+  log.info('/be-methods/downloads/createPersistent.js')
   onSuccess();
 };
