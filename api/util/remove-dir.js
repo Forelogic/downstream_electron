3b90f0dac42e710bc651b86b89b4119ed00fb0c0
KazukiSugiyama107 <sugiyama@forelogic.co.jp>
2021-01-15 20:46:06 +0900

    ログ追加

--- api/util/remove-dir.js
+++ api/util/remove-dir.js
@@ -4,8 +4,10 @@
 const fs = require('fs');
 const path = require('path');
 const downloadFileUtil = require("../downloads/download-file-util");
+const log = require('electron-log');
 
 function removeDir (dir, cb, ENOTEMPTY_attempts) {
+  log.info('remove-dir dir', dir)
   ENOTEMPTY_attempts = ENOTEMPTY_attempts || 0;
   const ENOTEMPTY_maxAttempts = 10;
   const ENOTEMPTY_TIMEOUT = 500;
