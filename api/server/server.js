ecbd1918a9e026216c70f4efbf19dc36efa797c2
tarui <tarui@forelogic.co.jp>
2023-05-31 18:41:33 +0900

    調査用ログ追加

--- api/server/server.js
+++ api/server/server.js
@@ -49,8 +49,12 @@ OfflineContentServer.prototype._startServer = function (port, callback) {
   }
 
   console.log('Server Path:', serverPath);
+  log.info('Server Path:', serverPath);
+  let script_test = path.join(serverPath, CHILD_SCRIPT_FILENAME);
   let script = path.join(app.getAppPath(), 'node_modules/downstream-electron', CHILD_SCRIPT_FILENAME);
   console.log('Script for server:', script);
+  log.info('script_test for server:', script_test);
+  log.info('Script for server:', script);
 
   //  FOR DEBUG PURPOSE self.childProcess = fork(script ,[],{execArgv:['--inspect=5860']});
   self.childProcess = fork(script, []);
