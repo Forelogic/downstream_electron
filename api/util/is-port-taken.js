e7da5132e2b388a06850cdffdb4833fcb50942b6
tarui <tarui@forelogic.co.jp>
2023-06-01 17:36:19 +0900

    調査用ログ追加

--- api/util/is-port-taken.js
+++ api/util/is-port-taken.js
@@ -1,15 +1,19 @@
 "use strict";
 
 const net = require('net');
+const log = require('electron-log');
 
 module.exports = function (port, fn) {
   const tester = net.createServer().once('error', function (err) {
+    log.info('is-port-taken err', err);
     if (err) {
       return fn(err);
     }
     fn(null, true);
   }).once('listening', function () {
+    log.info('is-port-taken listening');
     tester.once('close', function () {
+      log.info('is-port-taken close');
       fn(null, false);
     }).close();
   }).listen(port);
