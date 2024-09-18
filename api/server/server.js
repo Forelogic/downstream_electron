7fe9e0fcf7618a47e065962a9681d99016ad4883
ogawa-ma <ogawa@forelogic.co.jp>
2024-09-18 10:58:10 +0900

    startServer.jsが起動するよう修正

--- api/server/server.js
+++ api/server/server.js
@@ -49,8 +49,8 @@ OfflineContentServer.prototype._startServer = function (port, callback) {
   }
 
   console.log('Server Path:', serverPath);
-  let script = path.join(serverPath, CHILD_SCRIPT_FILENAME);
-  let script_o = path.join(app.getAppPath(), 'node_modules/downstream-electron', CHILD_SCRIPT_FILENAME);
+  let script_o = path.join(serverPath, CHILD_SCRIPT_FILENAME);
+  let script = path.join(app.getAppPath(), 'node_modules/downstream-electron', CHILD_SCRIPT_FILENAME);
   console.log('script_o = ', script_o);
   console.log('Script for server:', script);
 
